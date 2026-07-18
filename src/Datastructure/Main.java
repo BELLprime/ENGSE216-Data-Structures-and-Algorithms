5a4836543da1d9dbdec2e514832803f8bc55fe8d
BELLprime <natthakitrodruean@gmail.com>
2026-07-18 11:58:11 +0700

    Feat: currentQueue(show intdex front)

--- src/Datastructure/Main.java
+++ src/Datastructure/Main.java
@@ -1,11 +1,28 @@
 package Datastructure;
 public class Main {
     public static void main(String[] args) {
-        Queue_circlular q = new Queue_circlular(6);
-        q.enqueue(4);
+        Queue_circlular q = new Queue_circlular(3);
+        
         q.enqueue(5);
         q.enqueue(6);
-        q.show();
         q.enqueue(7);
+        q.showforloop();
+        System.out.println("Peek: " + q.currentQueue());
+        System.out.println("===================================");
+        System.out.println("Dequeue: " + q.dequeue()); 
+        System.out.println("Dequeue: " + q.dequeue()); 
+        q.showforloop();
+        System.out.println("Peek: " + q.currentQueue());
+        System.out.println("===================================");
+        q.enqueue(8);
+        q.showforloop();
+        System.out.println("Peek: " + q.currentQueue());
+        System.out.println("===================================");
+        System.out.println("Dequeue: " + q.dequeue()); 
+        q.showforloop();
+        System.out.println("Peek: " + q.currentQueue());
+        System.out.println("===================================");
+
+
     }
 }
