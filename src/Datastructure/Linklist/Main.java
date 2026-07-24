335e47df68402335f2120060ff97a322ad80bb47
BELLprime <natthakitrodruean@gmail.com>
2026-07-24 19:23:52 +0700

    LinkedList dynamic collection instead array.

---
+++ src/Datastructure/Linklist/Main.java
@@ -0,0 +1,13 @@
+package Datastructure.Linklist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ls = new LinkedList();
+        ls.add(12);
+        ls.add(11);
+        ls.add(13);
+        ls.showALl();
+        ls.addfirst(9);
+        ls.showALl();
+    }
+}
